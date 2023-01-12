60398a0ad9dab5a58734078be8bcb74a78959384
daijiromaeyama <daijiro.maeyama13@gmail.com>
2023-01-12 22:50:17 +0900

    Add Author model

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,13 +8,21 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator" do
-        argument :name, String, required: true
-      end
+    field :test_field, String, null: false, description: "An example field added by the generator" do
+      argument :name, String, required: true
+    end
+
     def test_field(name:)
       Rails.logger.info context[:time]
       "Hello #{name}!"
+    end
+
+    field :author, Types::AuthorType, null: true, description: "Returns one Author instance" do
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.where(id: id).first
     end
   end
 end
