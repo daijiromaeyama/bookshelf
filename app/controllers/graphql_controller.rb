62d6a26d273e36a7adfe2b72621f4fadf74d3206
daijiromaeyama <daijiro.maeyama13@gmail.com>
2023-01-16 00:31:32 +0900

    Add authorisation function

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,16 +2,18 @@ class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
+
+    session = Session.where(key: request.headers['Authorization']).first
+    Rails.logger.info "Logged in as #{session&.user&.email}"
+
     context = {
-      time: Time.now
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: session&.user
     }
     result = BookshelfSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
