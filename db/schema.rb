5391a897f94e16e668053ea2cee2e155dc1e858c
daijiromaeyama <daijiro.maeyama13@gmail.com>
2023-01-16 00:04:54 +0900

    Add login function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_132034) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_142357) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,4 +20,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_132034) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "is_superadmin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "sessions", "users"
 end
